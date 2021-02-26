c5ccdd7241ffbfb3a5def971a2e1645690660c2c
Morten Lyngberg Pedersen <mort73h9@edu.easj.dk>
2021-02-26 14:29:25 +0100

    added backing fields

--- BeerMorten/Beer.cs
+++ BeerMorten/Beer.cs
@@ -4,10 +4,33 @@ namespace BeerMorten
 {
     public class Beer
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Price { get; set; }
-        public int Abv { get; set; }
+        private int _id;
+        private string _name;
+        private int _price;
+        private int _abv;
 
+        public int Id
+        {
+            get => _id;
+            set { _id = value; }
+        }
+
+        public string Name
+        {
+            get => _name;
+            set { _name = value; }
+        }
+
+        public int Price
+        {
+            get => _price;
+            set { _price = value; }
+        }
+
+        public int Abv
+        {
+            get => _abv;
+            set { _abv = value; }
+        }
     }
 }
