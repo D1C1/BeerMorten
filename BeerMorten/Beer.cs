9cf9e2761b7c2605c0d1f76184a21624456ed751
Morten Lyngberg Pedersen <mort73h9@edu.easj.dk>
2021-02-26 14:27:48 +0100

    Added properties

--- BeerMorten/Beer.cs
+++ BeerMorten/Beer.cs
@@ -4,5 +4,10 @@ namespace BeerMorten
 {
     public class Beer
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Abv { get; set; }
+
     }
 }
